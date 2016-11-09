c258452a596d4645aa252a71e20ffcd81e0039a7
SpaTenDima <falaleev8@mail.ru>
2016-11-09 08:14:33 +0400

    Add files via upload

--- rev/js/myjs.js
+++ rev/js/myjs.js
@@ -1,7 +1,30 @@
-$(document).ready(function () {
 
-    
 
+$(function () {
+ var validator = $("#z16").validate({
+        rules: {
+            login_email: {
+                required: true,
+                email: true
+            },
+            login_name: {
+                required: true
+            }
+        }
+    })
+var arrId = ["#login_name", "#login_email"];
+$.each(arrId, function(indx, id) {
+        var next = $(id).next();
+        var fn = function() {
+            validator.element(id) ? next.removeClass('img_z16_mandatory').addClass('img_z16_check') : next.removeClass('img_z16_check').addClass('img_z16_mandatory')
+        };
+        $(id).on("input", fn);
+    }
+);
+});
+
+
+/*
     $('#z16').validate(function () {
         $.validate("#login_email", {
             rules: {
@@ -29,12 +52,9 @@ $(document).ready(function () {
         });
     });
 
-$( ".img_z16_mandatory" ).change(function() {
-  el.classList.remove('.img_z16_mandatory');
-  el.classList.add('.img_z16_check');
-  }
 
-});
+
+*/
 
 
 
